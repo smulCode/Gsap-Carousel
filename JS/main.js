db2c2dc295834d1aed8e8cf5b4e6b37875819127
smulCode <67417862+smulCode@users.noreply.github.com>
2021-12-24 15:27:27 +0100

    set up

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,32 @@
 // import {example} from "./modules/example.js"
-Draggable.create("#yourID");
+Draggable.create("#slide");
+
+
+
+Draggable.create("#slide", {
+    type:"x",
+    bounds: document.getElementById("carousel-container"),
+    inertia: true,
+    onClick: function() {
+        console.log("clicked");
+    },
+    onDragEnd: function() {
+        console.log("drag ended");
+    }
+});
+
+
+//Eelke
+// Draggble.create(invisibleDragElement, {
+//     bounds: {minX: -20, ,maxX: 20 },
+//     type: 'x',
+//     onDragEnd: (value) => checkIfDraggedEnoughToUpdateActiveIndex(value)
+//   })
+  
+//   checkIfDraggedEnoughToUpdateActiveIndex(value) {
+//     if (dragging <15) {
+//       activeIndex + 1;
+//     }
+  
+//     positionAllSlides();
+//   }
