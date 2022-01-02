4f60021b9b8371078992bc7daf3c1fcae4607d78
Wesley Smulders <67417862+smulCode@users.noreply.github.com>
2022-01-02 15:24:50 +0100

    touch buttons

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,8 @@
 const carousel = document.querySelector(".carousel-container");
 const track = document.querySelector(".carousel-track");
 const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel-button--right");
+const prevButton = document.querySelector(".carousel-button--left");
 const dotsNav = document.querySelector(".carousel-nav");
 const dots = Array.from(dotsNav.children);
 const images = [
@@ -20,27 +22,21 @@ const setSlidePosition = (slide, index) => {
 };
 
 slides.forEach(setSlidePosition);
+
 slides.forEach((slide, index) => {
   slide.style.backgroundImage = `url(${images[index]})`;
 });
 
-dotsNav.addEventListener("click", function (e) {
-  const targetDot = e.target.closest("button");
 
-  if (!targetDot) return;
-
-  const currentSlide = track.querySelector(".current-slide");
-  const currentDot = dotsNav.querySelector(".current-slide");
-  const targetIndex = dots.findIndex((dot) => dot === targetDot);
-  const targetSlide = slides[targetIndex];
-  const currentDotIndex = dots.findIndex((dot) => dot === currentDot);
+const moveToSlide = (track,currentSlide, targetSlide) => {
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+  const targetIndex = slides.findIndex((slide) => slide === targetSlide);
+  const currentSlideIndex = slides.findIndex((slide) => slide === currentSlide);
   const slideX = parseInt(targetSlide.style.left);
   carousel.style.backgroundImage = `url(${images[targetIndex]})`;
+  if (targetIndex < currentSlideIndex) {
 
-  updateDots(currentDot, targetDot);
-  setCurrentSlide(track, currentSlide, targetSlide);
-
-  if (targetIndex < currentDotIndex) {
     let tween = gsap.to(".carousel-slide", {
       x: -slideX,
       duration: 1.5,
@@ -62,7 +58,75 @@ dotsNav.addEventListener("click", function (e) {
     console.log("play");
   }
 
+};
+
+
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+  console.log(targetIndex);
+
+  if (targetIndex === 0) {
+    prevButton.classList.add("is-hidden");
+    nextButton.classList.remove("is-hidden");
+  } else if (targetIndex === slides.length - 1) {
+    prevButton.classList.remove("is-hidden");
+    nextButton.classList.add("is-hidden");
+  } else {
+    prevButton.classList.remove("is-hidden");
+    nextButton.classList.remove("is-hidden");
+  }
+};
+
+//When i click left ,move slides to the left
+prevButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const prevDot = currentDot.previousElementSibling;
+  const prevIndex = slides.findIndex((slide) => slide === prevSlide);
+
+  moveToSlide(track, currentSlide, prevSlide);
+  updateDots(currentDot, prevDot);
+  hideShowArrows(slides, prevButton, nextButton, prevIndex);
+});
+
+
+nextButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+  const nextIndex = slides.findIndex((slide) => slide === nextSlide);
   
+ 
+
+
+  moveToSlide(track, currentSlide, nextSlide);
+  updateDots(currentDot, nextDot);
+  hideShowArrows(slides, prevButton, nextButton, nextIndex);
+});
+
+
+
+
+
+
+
+dotsNav.addEventListener("click", function (e) {
+  const targetDot = e.target.closest("button");
+
+  if (!targetDot) return;
+
+  const currentSlide = track.querySelector(".current-slide");
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const targetIndex = dots.findIndex((dot) => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+ 
+
+  moveToSlide(track, currentSlide, targetSlide);
+  updateDots(currentDot, targetDot);
+  hideShowArrows(slides, prevButton, nextButton, targetIndex);
+
 });
 
 const setCurrentSlide = (currentSlide, targetSlide) => {
@@ -80,56 +144,32 @@ const updateDots = (currentDot, targetDot) => {
 
 
 
-// const activeDot = gsap.to(".active", {
-//   keyframes: {
-//     x: [0, 0, 15, 15, 15],
-//     y: [0, 10, 10, 10, 0],
-//     ease: "none",
-//     opacity:[1, 0.5, 0, 0, 1],
-//     stagger: 0.2,
-//   },
-//   duration: 0.15,
-// });
-
-// activeDot.restart();
-
-// const dotAnimation = gsap.timeline();
-// dotAnimation
-//   .to(".current-slide", {
-//     duration: 1,
-//     y: 0,
-//     padding: "1px 15px",
-//   })
-//   .set(".current-slide", {
-//     duration: 1,
-//     y: -2,
-//     padding: "1px 15px",
-//   })
-//   .to(".current-slide", {
-//     duration: 1,
-//     y: 0,
-//     padding: "1px 15px",
-//     opacity:0,
-//   })
-
-
-
 // Draggable.create("#track", {
 //     type:"x",
 //     // bounds: document.getElementById("main-container"),
 //     inertia: true,
+//     liveSnap: {
+//       points: [{x: (-slideWidth * 2), y: 0}],
+//       radius: 15,
+//   // we can also use this notation
+//   // points [{x: 0, y: 0}, {x: 30, y: 40}]
+//     },
+
 
 //     onClick: function() {
 //         console.log("clicked");
+//         console.log(this.getDirection());
       
 
 //     },
 //     onDragEnd: function() {
 //         console.log("drag ended");
-       
+//         console.log(this.getDirection());
 
 //     }
 // });
+
+
 
 //Eelke
 // Draggble.create(draggable-container, {
